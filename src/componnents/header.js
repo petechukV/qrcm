85cd30392cd2ef884424cde6ea825de87df47d80
petechukV <petechukvlad@gmail.com>
2019-02-21 00:05:07 +0200

    Testing slider as tag

--- src/componnents/header.js
+++ src/componnents/header.js
@@ -4,10 +4,13 @@ function Header() {
     return(
         <header>
             <section>
-               <a>
-                   <img>
-               </a>
+                <img src="http://svgshare.com/i/BKD.svg" alt="Logo"/>
+                <a href="https://github.com/petechukV/qrcm" target="_blank" rel="noopener noreferrer">
+                    <img src="http://svgshare.com/i/3zm.svg" alt="GitHub Repository"/>
+                </a>
             </section>
         </header>
-    )
+    );
 }
+
+export default Header;
