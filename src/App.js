0b8befbf9cc3db8ba80cfd2de5ee401ff49b3d49
Влад Петечук <you@example.com>
2019-02-20 22:51:47 +0200

    Start of test project
    That are test app for share file by using qr-code

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from '/componnents/header';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+import 'normalize.css/normaliza.css';
+import 'slider-animations.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+
+        </div>
+    );
+  }
+}
+
+export default App;
